5ae26aed3b52908c21c0ca921e6afb4254c3920b
d.sinyatkin <d.sinyatkin@2015-1.r37.blogdns.com>
2016-11-03 14:16:10 +0200

    22

---
+++ src/Parse.java
@@ -0,0 +1,37 @@
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author d.sinyatkin
+ */
+public class Parse {
+    
+    private Pattern myPattern;
+    
+    public Pattern getMyPattern() {
+        return myPattern;
+    }
+    public void setMyPattern(Pattern myPattern) {
+        this.myPattern = myPattern;
+    }
+        
+    public Pattern compilePattern(String pattern){
+        setMyPattern(Pattern.compile(pattern));
+        return getMyPattern();
+    }
+
+    
+    public String doMatcher(String line){
+        Matcher m = getMyPattern().matcher(line);
+        return = m.find() ? m.group(): null;
+    }
+    
+}
